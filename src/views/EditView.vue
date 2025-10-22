2c00ba8f28146ad08d5af56caf44bbc78af9682b
es215 <es215@NT1.local>
2025-10-22 14:46:26 +0900

    1stcommit

---
+++ src/views/EditView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="editor">
+        <h1>New Memo</h1>
+        <textarea name="memo" v-model="memoBody"></textarea>
+        <button>保存</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'EditView',
+  computed: {
+   memo: function(){
+    let id = this.$route.params["id"];
+    return this.$store.state.memos.find(memo => memo.id ==id);
+   }
+  }
+};
+</script>
---
+++ src/views/SecretView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="jurk">
+        <h1>下のボタンをクリックしてください</h1>
+        <button @click="infection">クリック</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SecretView',
+     methods: {
+    infection: function() {
+       alert("あなたのパソコンはウイルスに感染しました");
+    }
+  }
+    };
+ 
+</script>
